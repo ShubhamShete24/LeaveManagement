0fa62e27d22e9adf4f2fafc7dade4c8667564b8e
Arunangshu Biswas <arunangshu.biswas@microlise.com>
2023-03-11 20:48:42 +0530

    Configuration for running in development mode and production mode

---
+++ frontend/src/config.js
@@ -0,0 +1,8 @@
+const prod  = {
+    API_URL : ""
+}
+const dev = {
+    API_URL : "http://localhost:8000"
+}
+const config =  process.env.NODE_ENV === "development" ? dev : prod;
+export default  config;
