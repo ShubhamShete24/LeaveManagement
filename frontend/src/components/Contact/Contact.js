911180448aa54e64e938f9d5973b1a11b4fc083c
Shubham Shete <shubhamshete.ss.ss@gmail.com>
2023-03-23 20:43:03 +0530

    redux implimentation

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import config from '../../config';
+// import { useSelector } from 'react-redux';
+// import config from '../../config';
 
 function Contact() {
-  fetch(`${config.API_URL}/api`)
-    .then((res) => res.json())
-    .then((response) => {
-      console.log(response);
-    });
+  // fetch(`${config.API_URL}/api`)
+  //   .then((res) => res.json())
+  //   .then((response) => {
+  //     console.log(response);
+  //   });
+  // const userInfo = useSelector((s) => s.LoginUserDetailsReducer);
+  // console.log(userInfo);
   return <h1>This is contact page. Check console to see api calls</h1>;
 }
 
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { LoginUserDetails } from '../../redux/actions/loginUserDetailsActions';
 
 function Home() {
+  // const dispatch = useDispatch();
+  // dispatch(LoginUserDetails('Shubham'));
   return <div>Home</div>;
 }
 
