e702a5b694b700711653e6b578f1b5597dcfa6e4
Mayuri Chavan <mayurichavan66@gmail.com>
2023-04-10 08:36:59 +0530

    "Signup form"

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import mainLogo from '../../assets/images/5.jpg';
 import './Login.scss';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import mainLogo from '../../assets/images/5.jpg';
 
 export default function Login({ setIsSignUp, isSignUp }) {
   const handelClick = () => {
     setIsSignUp(!isSignUp);
+  };
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
   };
   return (
     <div className="containerr">
@@ -13,6 +26,32 @@ export default function Login({ setIsSignUp, isSignUp }) {
           <img src={mainLogo} alt="SteelSoft" />
         </div>
         <div className="loginForm">
+          {/* email */}
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
+            <OutlinedInput id="outlined-adornment-email" type="email" label="Email" />
+          </FormControl>
+          {/* password */}
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? 'text' : 'password'}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
           <button type="button" onClick={() => handelClick()}>
             signup
           </button>
