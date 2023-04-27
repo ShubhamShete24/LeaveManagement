119892c319742722ada86bc380482666c4fe49e2
mayuri0723 <mayurichavan66@gmail.com>
2023-04-27 14:11:16 +0530

    Sign Up form Changes

--- frontend/src/routes/Paths.js
+++ frontend/src/routes/Paths.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from '../pages/Auth/Login';
+// import Login from '../pages/Auth/Login';
 import Dashboard from '../pages/Dashboard/Dashboard';
+import Index from '../pages/Auth/Index';
 
 function Paths() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Login />} />
+        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/" element={<Index />} />
         <Route path="/dashboard/*" element={<Dashboard />} />
       </Routes>
     </div>
