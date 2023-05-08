e8a6002953e699635ce2dcdb945eb623131fe604
mayuri0723 <mayurichavan66@gmail.com>
2023-05-08 10:29:25 +0530

    "Holiday Page"

--- frontend/src/pages/Leave/HolidayPage.js
+++ frontend/src/pages/Leave/HolidayPage.js
@@ -4,16 +4,16 @@ import { makeStyles } from '@mui/styles';
 
 function HolidayPage() {
   const holidays = [
-    { name: "New Year's Day", date: '01/01/2023' },
-    { name: 'Martin Luther King Jr. Day', date: '01/17/2023' },
-    { name: "President's Day", date: '02/21/2023' },
-    { name: 'Memorial Day', date: '05/30/2023' },
-    { name: 'Independence Day', date: '07/04/2023' },
-    { name: 'Labor Day', date: '09/05/2023' },
-    { name: 'Columbus Day', date: '10/10/2023' },
-    { name: 'Veterans Day', date: '11/11/2023' },
-    { name: 'Thanksgiving Day', date: '11/24/2023' },
-    { name: 'Christmas Day', date: '12/25/2023' }
+    { name: "New Year's Day", date: '01/01' },
+    { name: 'Martin Luther King Jr. Day', date: '01/17' },
+    { name: "President's Day", date: '02/21' },
+    { name: 'Memorial Day', date: '05/30' },
+    { name: 'Independence Day', date: '07/04' },
+    { name: 'Labor Day', date: '09/05' },
+    { name: 'Columbus Day', date: '10/10' },
+    { name: 'Veterans Day', date: '11/11' },
+    { name: 'Thanksgiving Day', date: '11/24' },
+    { name: 'Christmas Day', date: '12/25' }
   ];
 
   const useStyles = makeStyles({
@@ -25,48 +25,54 @@ function HolidayPage() {
   });
 
   const classes = useStyles();
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
-  const filteredHolidays = holidays.filter((holiday) => new Date(holiday.date).getFullYear() === selectedYear);
-  const months = Array.from(new Set(filteredHolidays.map((holiday) => new Date(holiday.date).getMonth())));
+  const getHolidaysByMonth = () => {
+    const holidaysByMonth = Array.from(Array(12).keys()).map((month) => {
+      const monthHolidays = holidays.filter((holiday) => {
+        const holidayMonth = new Date(`2023-${holiday.date}`).getMonth();
+        return holidayMonth === month;
+      });
+      return {
+        month: new Date(`2023-${month + 1}-01`).toLocaleString('default', { month: 'long' }),
+        holidays: monthHolidays
+      };
+    });
 
-  const handleChangeYear = (event) => {
-    setSelectedYear(event.target.value);
+    return holidaysByMonth;
   };
 
   return (
     <div className="p-5">
       <div className="p-1">
         <Grid container spacing={2} justifyContent="center">
-          <Grid item xs={12} sx={{ textAlign: 'center' }}>
-            <Select label="Select Year" value={selectedYear} onChange={handleChangeYear}>
-              {[...new Array(10)].map((_, index) => (
-                <MenuItem key={index} value={new Date().getFullYear() + index}>
-                  {new Date().getFullYear() + index}
-                </MenuItem>
-              ))}
-            </Select>
-          </Grid>
-          {months?.map((month) => (
-            <Grid key={month} item xs={12} sm={4}>
+          {getHolidaysByMonth().map((month) => (
+            <Grid key={month.month} item xs={12} sm={4}>
               <Typography variant="h6" sx={{ marginBottom: 2 }}>
-                {new Date(
-                  filteredHolidays.find((holiday) => new Date(holiday.date).getMonth() === month).date
-                ).toLocaleString('default', { month: 'long' })}
+                {month.month}
               </Typography>
-              {filteredHolidays
-                .filter((holiday) => new Date(holiday.date).getMonth() === month)
-                ?.map((holiday) => (
-                  <Card key={holiday.date} variant="outlined" className={classes.card}>
-                    <CardContent>
-                      <Typography variant="h5" component="h2">
-                        {holiday.name}
+
+              {month.holidays.length > 0 ? (
+                <Card variant="outlined" className={classes.card}>
+                  <CardContent>
+                    {month.holidays.map((holiday) => (
+                      <Typography key={holiday.date} variant="body2" component="p">
+                        {holiday.name} -{' '}
+                        {new Date(`2023-${holiday.date}`).toLocaleDateString(undefined, {
+                          month: 'long',
+                          day: 'numeric'
+                        })}
                       </Typography>
-                      <Typography variant="body2" component="p">
-                        {new Date(holiday.date).toLocaleDateString()}
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                ))}
+                    ))}
+                  </CardContent>
+                </Card>
+              ) : (
+                <Card variant="outlined" className={classes.card}>
+                  <CardContent>
+                    <Typography variant="body2" component="p">
+                      No holiday
+                    </Typography>
+                  </CardContent>
+                </Card>
+              )}
             </Grid>
           ))}
         </Grid>
