789d4c93c3c5f9e5a8001b01da1208c999b7604c
Mayuri Chavan <mayurichavan66@gmail.com>
2023-05-10 00:27:50 +0530

    "MUI-data Grid Table"

--- frontend/src/routes/DashboardPaths.js
+++ frontend/src/routes/DashboardPaths.js
@@ -3,12 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Home from '../pages/Dashboard/Home';
 import Leaves from '../components/Leaves';
-import UserForm from '../pages/CreateUser/UserForm';
+// import UserForm from '../pages/CreateUser/UserForm';
 import PersonalDetailsForm from '../pages/CreateUser/PersonalDetailsForm';
 import EmploymentDetailsForm from '../pages/CreateUser/EmploymentDetailsForm';
 import HolidayPage from '../pages/Leave/HolidayPage';
 import LeaveSanctioner from '../pages/Leave/LeaveSanctioner';
 import { USER_INFO_KEY } from '../utils/constants';
+import Users from '../pages/CreateUser/Users';
 
 function DashboardPaths() {
   const sessionData =
@@ -22,7 +23,7 @@ function DashboardPaths() {
         <Route path="/holiday" element={<HolidayPage />} />
         {sessionData?.user[0]?.role[0]?.roleName === 'ADMIN' ? (
           <>
-            <Route path="/user" element={<UserForm />} />
+            <Route path="/user" element={<Users />} />
             <Route path="/personal-info" element={<PersonalDetailsForm />} />
             <Route path="/employee-details" element={<EmploymentDetailsForm />} />
           </>
