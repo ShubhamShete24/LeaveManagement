d4c9d21ec6a7ee80ad19bbfe725877b745196620
Arunangshu37 <109015710+Arunangshu37@users.noreply.github.com>
2023-05-13 19:17:48 +0530

    Refactoration of leaves component and user creation api fix (facilitate creation of leave balance) (#14)
    * Modified code to handle leave balance creation when new user is created
    
    * Refactor code for leave management
    
    * Corrected action buton not being disabled after leave sanctioned

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,22 +1,12 @@
 import { combineReducers } from 'redux';
 import LoginUserDetailsReducer from './loginUserDetailsReducer';
-import {
-  GetLeaveTypesReducer,
-  ApplyForLeavesReducer,
-  GetAppliedLeavesReducer,
-  UpdateLeaveApplicationReducer,
-  GetLeaveBalancesReducer
-} from './leaveReducer';
+import { LeaveReducer } from './leaveReducer';
 import GetHolidaysReducer from './holidayReducer';
 import UserDetailReducers from './userDetailReducer';
 
 export default combineReducers({
   LoginUserDetailsReducer,
-  GetLeaveTypesReducer,
-  ApplyForLeavesReducer,
   GetHolidaysReducer,
-  GetAppliedLeavesReducer,
-  UpdateLeaveApplicationReducer,
   UserDetailReducers,
-  GetLeaveBalancesReducer
+  LeaveReducer
 });
